25b9f767ef4fe15c478bd0fea8480ebc5675af85
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2023-03-05 21:38:32 +0300

    TopBar completed for mobile

--- components/TopBar/TopBar.tsx
+++ components/TopBar/TopBar.tsx
@@ -1,18 +1,42 @@
+"use client";
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import Menu from "../Menu/Menu";
 import styles from "./TopBar.module.css";
+
 export default function TopBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  function clickMenuIcon() {
+    setMenuOpen((menuOpen) => !menuOpen);
+  }
+
+  function clickMenuArea() {
+    setMenuOpen((menuOpen) => !menuOpen);
+  }
+
+  const customStyleForMenu = { display: menuOpen ? "block" : "none" };
+
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.title}>www.serdarserifoglu.com</div>
-      <div className={styles.menuicon}>
-        <Image
-          src="/icons/grid-lines.svg"
-          alt="Grip Icon"
-          width={20}
-          height={9}
-          priority
-        />
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.title}>
+          <Link href="/"> www.serdarserifoglu.com</Link>
+        </div>
+        <div className={styles.menuicon} onClick={clickMenuIcon}>
+          <Image
+            src="/icons/grid-lines.svg"
+            alt="Grip Icon"
+            width={20}
+            height={9}
+            priority
+          />
+        </div>
       </div>
-    </div>
+      <div onClick={clickMenuArea} style={customStyleForMenu}>
+        <Menu />
+      </div>
+    </>
   );
 }
