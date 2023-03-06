0cea243b900a9cc3d31e4f0ea97f9c913fbedff2
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2023-03-06 10:12:01 +0300

    Sidebar links was completed

--- components/SideBar/SideBar.tsx
+++ components/SideBar/SideBar.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import Menu from "../Menu/Menu";
 import styles from "./SideBar.module.css";
 
@@ -6,24 +7,34 @@ export default function SideBar() {
   return (
     <div className={styles.wrapper}>
       <div className={styles.title}>
-        <h2 className={styles.name}>Ömer Serdar ŞERİFOĞLU</h2>
-        <div className={styles.jobtitle}>Software Developer</div>
+        <Link className={styles.link} href="/">
+          <h2 className={styles.name}>Ömer Serdar ŞERİFOĞLU</h2>
+
+          <div className={styles.jobtitle}>Software Developer</div>
+        </Link>
         <br />
         <div className={styles.icons}>
-          <Image
-            src="/icons/mdi_linkedin.svg"
-            alt="Grip Icon"
-            width={32}
-            height={32}
-            priority
-          />
-          <Image
-            src="/icons/mdi_github.svg"
-            alt="Grip Icon"
-            width={32}
-            height={32}
-            priority
-          />
+          <Link
+            href="https://www.linkedin.com/in/omer-serdar-serifoglu/"
+            target="_blank"
+          >
+            <Image
+              src="/icons/mdi_linkedin.svg"
+              alt="Grip Icon"
+              width={32}
+              height={32}
+              priority
+            />
+          </Link>
+          <Link href="https://github.com/SerdarSerifoglu" target="_blank">
+            <Image
+              src="/icons/mdi_github.svg"
+              alt="Grip Icon"
+              width={32}
+              height={32}
+              priority
+            />
+          </Link>
         </div>
       </div>
       <div className={styles.menu}>
