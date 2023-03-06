6e41b97bad06acd3651caf90ae539e80ea20813b
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2023-03-06 14:12:44 +0300

    Home page completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
+import styles from "./page.module.css";
 export default function Home() {
-  return <div>ÖMER SERDAR</div>;
+  return (
+    <>
+      <div className={styles["main-content"]}>
+        <q>
+          <i>Bildiğiniz her şeyi mutlaka bir başkasından öğrenmişsinizdir.</i>
+        </q>
+        <br />
+        <p className={styles.author}>
+          Joshua Wooden{" "}
+          <span className={styles.info}>
+            (Efsane basketbol antrenörü John Wooden'ın babası)
+          </span>
+        </p>
+      </div>
+    </>
+  );
 }
